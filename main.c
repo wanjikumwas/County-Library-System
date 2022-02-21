fd33e2bf9885a98137d553a2bdf0e6822d1ca557
wanjikumwas <99952287+wanjikumwas@users.noreply.github.com>
2022-02-21 19:22:04 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+Project: county Library Management System
+Author:  Wanjiku Mwangi
+Compiler:GNU GCC
+Language:C99
+license:MIT
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\County Library\n");
+    printf("Welcome Ms Wanjiku\n");
+    printf("What would you like to do?\n");
+    printf("1.Add new user\n");
+    printf("2.Add Book.\n");
+    printf("Selected action: ");
+    return 0;
+}
